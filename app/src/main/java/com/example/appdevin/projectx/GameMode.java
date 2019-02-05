e45e6ec896372ebebde2451a82bc117d8e4df57e
Jagateesvaran <41919463+Jagateesvaran@users.noreply.github.com>
2019-02-05 13:44:57 +0800

    Add files via upload

--- app/src/main/java/com/example/appdevin/projectx/GameMode.java
+++ app/src/main/java/com/example/appdevin/projectx/GameMode.java
@@ -4,15 +4,29 @@ import android.graphics.Typeface;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class GameMode extends AppCompatActivity {
 
     TextView txtTitle,txtTimer;
     Typeface myFont;
     CountDownTimer countDownTimer;
+
+    EditText editText;
+    ListView listView;
+    Button button;
+    ArrayList<String> arrayList =  new ArrayList<>();;
+    ArrayAdapter arrayAdapter;
+    // store fruit
+    String[] fruitList = {"APPLE", "ORANGE", "PEAR"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +39,37 @@ public class GameMode extends AppCompatActivity {
         initFont();
 
         startTimer(30);
+
+        arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, arrayList);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user_input = editText.getText().toString();
+
+
+                for (int x = 0; x < 2; x++) {
+                    if (user_input.equals(fruitList[x])) {
+                        arrayList.add(user_input);
+                        listView.setAdapter(arrayAdapter);
+                    }else{
+                        Toast.makeText(GameMode.this, "Not In the list", Toast.LENGTH_SHORT).show();
+                    }
+
+
+                }
+            }
+        });
+
     }
 
     void init(){
         txtTitle=findViewById(R.id.txtTitle);
         txtTimer=findViewById(R.id.txtTimer);
+        listView = findViewById(R.id.listViewMain);
 
+        button = findViewById(R.id.button2);
+        editText = findViewById(R.id.editText);
     }
 
     void initFont(){
